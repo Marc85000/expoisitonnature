46cdb39f8584faaf4f6914f75ecc18707e08e2ff
Marc85000 <zeroxinvictus@gmail.com>
2020-01-08 15:41:39 +0100

    initial

---
+++ db/migrate/20200108091824_create_arts.rb
@@ -0,0 +1,14 @@
+class CreateArts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :arts do |t|
+      t.string :author
+      t.text :description
+      t.string :title
+      t.string :date
+      t.string :dimenions
+      t.string :photo
+
+      t.timestamps
+    end
+  end
+end
